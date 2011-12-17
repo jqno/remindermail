531924cfc469a72765c8f639fe7c7a05d307f23e
Jan Ouwens <jqno.android@gmail.com>
2011-12-17 10:32:29 +0100

    Second activity with separate icon

---
+++ src/main/scala/MailActivity.scala
@@ -0,0 +1,10 @@
+package nl.jqno.quickmail
+
+import android.app.Activity
+import android.os.Bundle
+
+class MailActivity extends Activity {
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+  }
+}
