1bb2fa4484a0781d664162f9d1b1b6bf5bc89889
Jan Ouwens <jqno.android@gmail.com>
2011-12-17 22:04:37 +0100

    Add to sharebox

--- src/main/scala/MailActivity.scala
+++ src/main/scala/MailActivity.scala
@@ -18,10 +18,23 @@ class MailActivity extends Activity {
 
   private def mail {
     val recipient = "%s <%s>".format(state.name.get, state.mail.get)
+    val (subject, text) = determineContent
+
     val intent = new Intent(Intent.ACTION_SEND)
     intent.setType("plain/text")
     intent.putExtra(Intent.EXTRA_EMAIL, Array(recipient))
+    intent.putExtra(Intent.EXTRA_SUBJECT, subject)
+    intent.putExtra(Intent.EXTRA_TEXT, text)
+
     startActivity(intent)
+  }
+
+  private def determineContent: (String, String) = {
+    val intent = getIntent
+    if (intent.getAction == Intent.ACTION_SEND)
+      (intent.getStringExtra(Intent.EXTRA_SUBJECT), intent.getStringExtra(Intent.EXTRA_TEXT))
+    else
+      ("", "")
   }
 
   private def configure {
