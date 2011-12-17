a90a9f408e302afec3d5d2a79b186a66863283e2
Jan Ouwens <jqno.android@gmail.com>
2011-12-17 17:38:25 +0100

    Start e-mail intent

--- src/main/scala/MailActivity.scala
+++ src/main/scala/MailActivity.scala
@@ -1,10 +1,31 @@
 package nl.jqno.quickmail
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 
 class MailActivity extends Activity {
+  private lazy val state = new State(this)
+
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
+    state.mail match {
+      case Some(_) => mail
+      case None    => configure
+    }
+    finish()
+  }
+
+  private def mail {
+    val recipient = "%s <%s>".format(state.name.get, state.mail.get)
+    val intent = new Intent(Intent.ACTION_SEND)
+    intent.setType("plain/text")
+    intent.putExtra(Intent.EXTRA_EMAIL, Array(recipient))
+    startActivity(intent)
+  }
+
+  private def configure {
+    val intent = new Intent(this, classOf[ConfigActivity])
+    startActivity(intent)
   }
 }
--- src/main/scala/State.scala
+++ src/main/scala/State.scala
@@ -1,13 +1,13 @@
 package nl.jqno.quickmail
 
-import android.app.Activity
 import android.content.Context
 
-class State(val activity: Activity) {
+class State(val ctx: Context) {
+  private val SETTINGS_ID = "state"
   private val NAME_ID = "name"
   private val MAIL_ID = "mail"
 
-  private val prefs = activity.getPreferences(Context.MODE_PRIVATE)
+  private val prefs = ctx.getSharedPreferences(SETTINGS_ID, Context.MODE_PRIVATE)
 
   def name = Option(prefs.getString(NAME_ID, null))
   def name_=(name: String) = set(NAME_ID, name)
