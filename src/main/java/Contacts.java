669028103461e2cada2aa6ed6b481a400e7f36b5
Jan Ouwens <jqno.android@gmail.com>
2011-12-18 14:13:59 +0100

    Applied GPLv3

--- src/main/java/Contacts.java
+++ src/main/java/Contacts.java
@@ -1,3 +1,24 @@
+/*
+ * QuickMail is an Android app that will let you quickly send e-mail to
+ * a pre-defined address.
+ *
+ * Copyright (C) 2011 by Jan Ouwens
+ *
+ * This file is part of QuickMail.
+ *
+ * QuickMail is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QuickMail is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QuickMail.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package nl.jqno.quickmail;
 
 import android.net.Uri;
