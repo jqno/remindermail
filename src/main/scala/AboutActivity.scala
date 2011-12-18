eef511cd1b3152926be77e7e6ef91e7893a6c868
Jan Ouwens <jqno.android@gmail.com>
2011-12-18 13:44:01 +0100

    Content and layout improvements in About activity

--- src/main/scala/AboutActivity.scala
+++ src/main/scala/AboutActivity.scala
@@ -2,7 +2,8 @@ package nl.jqno.quickmail
 
 import android.app.Activity
 import android.os.Bundle
-import android.text.util.Linkify
+import android.text.Html
+import android.text.method.LinkMovementMethod
 import android.widget._
 
 import FindView._
@@ -11,7 +12,14 @@ class AboutActivity extends Activity with FindView {
   override def onCreate(savedInstanceState: Bundle) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.about)
-    Linkify.addLinks(findView[TextView](R.id.about_website), Linkify.WEB_URLS)
+    linkify(findView[TextView](R.id.about_explanation))
+    linkify(findView[TextView](R.id.about_website))
     findView[Button](R.id.about_close).onClick { _ => finish }
+  }
+
+  private def linkify(textView: TextView) {
+    textView.setMovementMethod(LinkMovementMethod.getInstance)
+    val text = textView.getText.toString
+    textView.setText(Html.fromHtml(text))
   }
 }
