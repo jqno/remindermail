3632620b7a47d87fcbefea0d5e052d52138eded0
Jan Ouwens <jan.ouwens@gmail.com>
2017-02-11 11:56:59 +0100

    Adds privacy policy

--- src/main/scala/nl/jqno/remindermail/AboutActivity.scala
+++ src/main/scala/nl/jqno/remindermail/AboutActivity.scala
@@ -36,6 +36,7 @@ class AboutActivity extends Activity with FindView {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.about)
     linkify(findView[TextView](R.id.about_explanation))
+    linkify(findView[TextView](R.id.about_privacy))
     linkify(findView[TextView](R.id.about_website))
     findView[Button](R.id.about_close) onClick finish()
     find(R.id.about_line).setBackgroundResource(state.color)
